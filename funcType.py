c76e63f260dcc219685b278eafc381fe050ad8ae
lamontu <lamontyu.163.com>
2016-10-25 17:50:13 +0800

    add function and method type test

---
+++ funcType.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+class A(object):
+    @staticmethod
+    def staticMethod():
+        print('staticFunc()')
+    
+    @classmethod
+    def classMethod():
+        print('classMethod()')
+
+    def func():
+        print('func()')
+
+a = A()
+print('A.staticMetho:', A.staticMethod)
+print('A.classMethod:', A.classMethod)
+print('A.func:', A.func)
+
+print('a.staticMethod:',a.staticMethod)
+print('a.classMethod:', a.classMethod)
+print('a.func:', a.func)
+
+print()
+
+print(type(A.staticMethod))
+print(type(A.classMethod))
+print(type(A.func))
+
+print(type(a.staticMethod))
+print(type(a.classMethod))
+print(type(a.func))
