775a54aa654344a3cce0724704620c75d3ad0e77
lamontu <lamontyu@163.com>
2016-10-26 17:52:09 +0800

    add a decorator demo file

---
+++ decorator_performance.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import time, functools
+
+def performance(f):
+    def fn(*args, **kw):
+        t_start = time.time()
+        result = f(*args, **kw)
+        t_end = time.time()
+        print('call %s() in %fs' % (f.__name__, t_end - t_end))
+        return result
+    return fn
+
+@performance
+def factorial(n):
+    return functools.reduce(lambda x, y: x * y, range(1, n + 1))
+
+print(factorial(10))
