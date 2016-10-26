fc8e855b55310a1580431aa31ab985cecb17597d
lamontu <lamontyu@163.com>
2016-10-26 16:55:06 +0800

    add a decorator test file

--- decorator_1.py
+++ decorator_1.py
@@ -1,25 +1,20 @@
-import time, functools
+# -*- coding: utf-8 -*-
 
-def performance(unit):
-    def perf_decorator(f):
-        @functools.wraps(f)
-        def wrapper(*args, **kw):
-            t1 = time.time()
-            r = f(*args, **kw)
-            t2 = time.time()
-            if unit=='ms':
-                t = (t2 - t1) * 1000
-            else:
-                t = t2 - t1
-            #t = (t2 - t1) * 1000 if unit=='ms' esle (t2 - t1)
-            print('call %s() in %f %s'% (f.__name__, t, unit))
-            return r
-        return wrapper
-    return perf_decorator
+def deco(func):
+    def in_deco():
+        print('in deco')
+        func()
+    print('call deco')
+    return in_deco
 
-@performance('ms')
-def factorial(n):
-    return functools.reduce(lambda x,y: x*y, range(1, n+1))
+@deco
+def bar():
+    print('in bar')
 
-print(factorial(10))
-print(factorial.__name__)
+print()
+print('type(bar) <<<<')
+print(type(bar))
+
+print()
+print('bar() <<<<')
+bar()
