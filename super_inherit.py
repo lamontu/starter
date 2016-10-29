f2e1ac7d7ed870985bd959ff4aca9435b4d51a2d
lamontu <lamontyu@163.com>
2016-10-29 17:21:43 +0800

    add mro inspect

--- super_inherit.py
+++ super_inherit.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import inspect
+
 
 class Base(object):
     def __init__(self):
@@ -20,8 +22,20 @@ class Medium2(Base):
 
 class Leaf(Medium1, Medium2):
     def __init__(self):
-        super(Leaf, self).__init__() 
+        """
+        mro = [Leaf, Medium1, Medium2, Base]
+        super(type, obj).__init__() 
+        search start from the one next to type 
+        """        
+        super(Leaf, self).__init__()  # search from Medium1 
+        # super(Medium1, self).__init__()  # search from Medium2 
+        # super(Medium2, self).__init__()  # search from Base 
+
         print('Leaf init')
 
 
+print('inspect.getmro(Leaf) >>>>')
+print(inspect.getmro(Leaf))
+
+print('leaf = Leaf() >>>>')
 leaf = Leaf()
