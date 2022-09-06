16bfd5c1c3eeed7302ef741b8e622f1ea71f71c8
lamontu <lamontyu@163.com>
2022-09-06 22:18:16 +0800

    generator example

---
+++ iterator_iterable.py
@@ -0,0 +1,28 @@
+
+def is_subsequence_(a, b):
+    b = iter(b)
+    return all(i in b for i in a)
+
+# same as above is_subsequence_
+def is_subsequence(a, b):
+    gen = (i for i in a)
+    print(gen)
+
+    # iterable object a, can be consumed again in the last return line
+    for i in gen:
+        print(i)
+
+    b = iter(b)
+    print(b)
+
+    gen = ((i in b) for i in a)
+    print(gen)
+
+    # iterator b, can not be consumed again in the last return line
+    # for i in gen:
+    #     print(i)
+
+    return all(((i in b) for i in a))
+
+print(is_subsequence([1, 3, 5], [1, 2, 3, 4, 5]))
+print(is_subsequence([1, 4, 3], [1, 2, 3, 4, 5]))
