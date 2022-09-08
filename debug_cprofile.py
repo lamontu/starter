f0062cc165ab36e64ac3f83708a1969a7b3a17cf
lamontu <lamontyu@163.com>
2022-09-08 22:36:39 +0800

    add debug tool examples

---
+++ debug_cprofile.py
@@ -0,0 +1,29 @@
+
+def memoize(f):
+    memo = {}
+    def helper(x):
+        if x not in memo:
+            memo[x] = f(x)
+        return memo[x]
+    return helper
+
+# add this decorator to decrease the call count
+@memoize
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+def fib_seq(n):
+    res = []
+    if n > 0:
+        res.extend(fib_seq(n-1))
+    res.append(fib(n))
+    return res
+
+
+import cProfile
+cProfile.run('fib_seq(30)')
